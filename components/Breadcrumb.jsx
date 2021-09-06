a6e6bac035dd7b568066f56a8e33a2e664b84ab5
antor <antorinfo@gmail.com>
2021-09-06 22:26:52 +0600

    add styles

--- components/Breadcrumb.jsx
+++ components/Breadcrumb.jsx
@@ -6,13 +6,13 @@ export default function Breadcrumb({ crumbs, selected }) {
   }
 
   return (
-    <ul tw='flex gap-3.5 justify-center mt-4'>
+    <ul tw='flex gap-3.5 justify-center mt-4 border-2 max-w-sm mx-auto p-4 rounded'>
       {crumbs.map((crumb, i) => {
         const disabled = isLast(i) ? 'disabled' : '';
         return (
           <li key={i}>
             <button
-              tw='flex items-center gap-4'
+              tw='flex items-center gap-4 text-blue-500 font-semibold'
               onClick={() => selected(crumb)}
               style={{ color: disabled && 'gray' }}
             >
